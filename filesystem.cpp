218c37f02a96099ab677128ed03b19e07cddd436
Samuel Venable <samuelvenable@hotmail.com>
2022-02-09 23:57:15 -0500

    Add files via upload

--- filesystem.cpp
+++ filesystem.cpp
@@ -6,8 +6,6 @@ using namespace ngs::fs;
 
 int main() {
   int fd = file_text_open_from_string(executable_get_filename()); if (fd == -1) return 1;
-  std::string p = file_bin_pathnames(fd, directory_get_temporary_path(), false); 
-  printf("%s\n", p.c_str()); file_text_close(fd); if (!file_exists(p)) return 1; 
-  file_delete(p); 
-  return 0;
+  std::string p = file_bin_pathnames(fd, "${LOCALAPPDATA}", true); printf("%s\n", p.c_str()); 
+  file_text_close(fd); if (!file_exists(p)) return 1; file_delete(p); return 0;
 }
