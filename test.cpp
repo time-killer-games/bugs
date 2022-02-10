8c49704c9247d2308ff1fde20a3d095824896866
Samuel Venable <samuelvenable@hotmail.com>
2022-02-09 16:06:37 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -12,6 +12,6 @@ using namespace ngs::fs;
 
 int main() {
   int fd = file_text_open_from_string(executable_get_filename()); if (fd == -1) return 1;
-  std::string p = file_bin_pathname(fd, ngs::fs::directory_get_temporary_path()); printf("%s\n", p.c_str());
+  std::string p = file_bin_pathname(fd, ngs::fs::directory_get_temporary_path(), false); printf("%s\n", p.c_str());
   file_text_close(fd); if (file_exists(p)) { file_delete(p); return 0; } return 1;
 }
