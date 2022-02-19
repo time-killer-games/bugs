effe4b1a9ae4134f804cfef0b5b0b4bd567c27fb
Samuel Venable <samuelvenable@hotmail.com>
2022-02-19 20:43:52 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd "${0%/*}";
 if [ $(uname) = "Darwin" ]; then
-  clang++ "apiprocess/process.cpp" "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body;
+  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body;
 elif [ $(uname) = "Linux" ]; then
-  g++ "apiprocess/process.cpp" "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++;
+  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "apiprocess/process.cpp" "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
+  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "apiprocess/process.cpp" "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -lc;
+  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -lc;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ "apiprocess/process.cpp" "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
+  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
 else
   g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem.exe" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -static;
 fi;
