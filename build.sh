fa95926beb4488d9d835bcb44e31164edf29ab60
Samuel Venable <samuelvenable@hotmail.com>
2022-02-22 17:58:44 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ cd "${0%/*}";
 if [ $(uname) = "Darwin" ]; then
   clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body;
 elif [ $(uname) = "Linux" ]; then
-  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++;
+  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -lpthread;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
+  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc -lpthread;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -lc;
+  g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -lc -lpthread;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc;
+  clang++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem" -std=c++20 -Wno-empty-body -lc -lpthread;
 else
   g++ "apifilesystem/filesystem.cpp" "filesystem.cpp" -o "filesystem.exe" -std=c++20 -Wno-empty-body -static-libgcc -static-libstdc++ -static;
 fi;
