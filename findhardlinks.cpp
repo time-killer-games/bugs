0e7c81db9003c5a33e5117dfc9ab4ef7eedc6610
Samuel Venable <samuelvenable@hotmail.com>
2022-03-03 23:07:59 -0500

    Add files via upload

--- findhardlinks.cpp
+++ findhardlinks.cpp
@@ -16,6 +16,8 @@ using std::wstring;
 
 namespace {
 
+  /* read, write, append 
+  open() permissions. */
   enum {
     FD_RDONLY,
     FD_WRONLY,
@@ -24,6 +26,8 @@ namespace {
     FD_RDAP
   };
 
+  /* necessary in GUI windows applications to process window
+  clicks without crashing during lasting for/while loops. */
   void message_pump() {
     #if defined(_WIN32) 
     MSG msg; while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
@@ -34,17 +38,20 @@ namespace {
   }
 
   #if defined(_WIN32) 
+  // UTF-8 support on Windows: string to wstring.
   wstring widen(string str) {
     size_t wchar_count = str.size() + 1; vector<wchar_t> buf(wchar_count);
     return wstring{ buf.data(), (size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count) };
   }
 
+  // UTF-8 support on Windows: wstring to string.
   string narrow(wstring wstr) {
     int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), nullptr, 0, nullptr, nullptr); vector<char> buf(nbytes);
     return string{ buf.data(), (size_t)WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), buf.data(), nbytes, nullptr, nullptr) };
   }
   #endif
 
+  // replace all substrings with new substring in string.
   string string_replace_all(string str, string substr, string nstr) {
     size_t pos = 0;
     while ((pos = str.find(substr, pos)) != string::npos) {
@@ -55,6 +62,7 @@ namespace {
     return str;
   }
 
+  // path without filename spec.
   string filename_path(string fname) {
     #if defined(_WIN32)
     size_t fp = fname.find_last_of("\\/");
@@ -65,6 +73,7 @@ namespace {
     return fname.substr(0, fp + 1);
   }
 
+  // path without directory path spec.
   string filename_name(string fname) {
     #if defined(_WIN32)
     size_t fp = fname.find_last_of("\\/");
@@ -75,6 +84,7 @@ namespace {
     return fname.substr(fp + 1);
   }
 
+  // get temporary directory.
   string directory_get_temporary_path() {
     std::error_code ec;
     string result = findhardlinks::fs::temp_directory_path(ec).string();
@@ -85,6 +95,7 @@ namespace {
     return (ec.value() == 0) ? result : "";
   }
 
+  // write string to file descriptor.
   long file_write_string(int fd, string str) {
     char *buffer = str.data();
     #if defined(_WIN32)
@@ -95,6 +106,8 @@ namespace {
     return result;
   }
 
+  /* create a temporary file with the string contents str.
+  filename suffix - replaces XXXXXX in buffer randomly. */
   int file_open_from_string(string str) {
     string fname = directory_get_temporary_path() + "temp.XXXXXX";
     #if defined(_WIN32)
@@ -117,6 +130,7 @@ namespace {
     return fd;
   }
 
+  // open file descriptor with mode (read, write, append, etc).
   int file_open(string fname, int mode) {
     #if defined(_WIN32)
     wstring wfname = widen(fname);
@@ -147,6 +161,7 @@ namespace {
     return -1;
   }
 
+  // close file descriptor.
   int file_close(int fd) {
     #if defined(_WIN32)
     return _close(fd);
@@ -155,6 +170,7 @@ namespace {
     #endif
   }
 
+  // check if regular file (non-directory) exists.
   bool file_exists(string fname) {
     std::error_code ec;
     const findhardlinks::fs::path path = findhardlinks::fs::path(fname);
@@ -162,6 +178,7 @@ namespace {
       (!findhardlinks::fs::is_directory(path, ec)) && ec.value() == 0);
   }
 
+  // delete file or hardlink.
   bool file_delete(string fname) {
     std::error_code ec;
     if (!file_exists(fname)) return false;
@@ -169,6 +186,7 @@ namespace {
     return (findhardlinks::fs::remove(path, ec) && ec.value() == 0);
   }
 
+  // check if directory exists.
   bool directory_exists(string dname) {
     std::error_code ec;
     const findhardlinks::fs::path path = findhardlinks::fs::path(dname);
@@ -176,12 +194,14 @@ namespace {
       findhardlinks::fs::is_directory(path, ec) && ec.value() == 0);
   }
 
+  // create directory recursively.
   bool directory_create(string dname) {
     std::error_code ec;
     const findhardlinks::fs::path path = findhardlinks::fs::path(dname);
     return (findhardlinks::fs::create_directories(path, ec) && ec.value() == 0);
   }
 
+  // rename or move file to name and/or destination.
   bool file_rename(string oldname, string newname) {
     std::error_code ec;
     if (!file_exists(oldname)) return false;
@@ -193,6 +213,7 @@ namespace {
     return (ec.value() == 0);
   }
 
+  // create hardlink.
   bool hardlink_create(string fname, string newname) {
     if (file_exists(fname)) {
       if (!directory_exists(filename_path(newname)))
@@ -212,49 +233,59 @@ namespace {
 
 } // anonymous namespace
 
+// our test case:
 int main(int argc, char **argv) {
   int fd = file_open_from_string(filename_name(argv[0] ? argv[0] : "(null)")); 
 
   if (fd == -1) { 
-    return 1; 
+    return 1; // failure
   }
 
   vector<string> dnames;
   dnames.push_back(directory_get_temporary_path());
   vector<string> p = findhardlinks::findhardlinks(fd, dnames, false);
+  /* close fist because Windows won't allow us to 
+  move/rename it when opened; win32-only issue */
   file_close(fd); 
 
   if (p.empty()) {
-    return 1;
+    return 1; // failure
   }
 
+  // rename original file to have hardlink number
   vector<string> p2;
   p2.push_back(p[0] + " - hardlink 00");
   file_rename(p[0], p2[0]); 
 
+  // if file exists, create 99 hardlinks
   if (file_exists(p2[0])) {
     for (unsigned i = 1; i < 100; i++) {
       if (!hardlink_create(p2[0], p[0] + " - hardlink " + ((std::to_string(i).length() == 1) ?  ("0" + std::to_string(i)) : std::to_string(i)))) {
-        return 1;
+        return 1; // failure
       }
     }
   }
 
+  // open original hardlink again, read-only
   fd = file_open(p2[0], FD_RDONLY);
 
   if (fd == -1) {
     return 1;
   }
 
+  // get hardlinks
   p2 = findhardlinks::findhardlinks(fd, dnames, false);
   file_close(fd); 
 
+  // sort alphabetically, numerically
   std::sort(p2.begin(), p2.end()); 
 
+  // print hardlink filenames, then delete them.
   for (unsigned i = 0; i < p2.size(); i++) { 
     printf("%s\n", p2[i].c_str()); 
     file_delete(p2[i]); 
   } 
 
+  // done:
   return 0;
 }
